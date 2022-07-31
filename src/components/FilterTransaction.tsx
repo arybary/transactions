2c4bbe9d119c8b93aa68af12c5497406ef56bde5
arybary <l3fljl5ldydy@gmail.com>
2022-07-31 12:50:54 +0300

    all

--- src/components/FilterTransaction.tsx
+++ src/components/FilterTransaction.tsx
@@ -18,31 +18,31 @@ const FilterTransaction: React.FC = () => {
   };
   const styleFilter = {
     height: "40px",
-    backgroundColor: "silver",
-    border: "thick double white",
+    backgroundColor: "silver",   
     fontWeight: "bold",
   };
 
-  const styleDefaultFilter = {
+  const styleNameFilter = {
     backgroundColor: "green",
     color: "white",
     fontWeight: "bold",
+    margin:"5px",
+    padding:"10px",
+    border: "thick double white",
   };
   return (
     <>
       <form onChange={handleSubmit(onSubmit)}>
+        <label style={styleNameFilter}> Filter by Status:</label>
         <select style={styleFilter} {...register("filterStatus")}>
-          <option value="" style={styleDefaultFilter}>
-            Filter by Status
-          </option>
+          <option value="">none</option>
           <option value="Pending">Pending</option>
           <option value="Completed">Completed</option>
           <option value="Cancelled">Cancelled</option>'Status Completed'
         </select>
+        <label style={styleNameFilter}> Filter by Type:</label>
         <select style={styleFilter} {...register("filterType")}>
-          <option value="" style={styleDefaultFilter}>
-            Filter by Type
-          </option>
+          <option value="">none</option>
           <option value="Withdrawal">Withdrawal</option>
           <option value="Refill">Refill</option>
         </select>
